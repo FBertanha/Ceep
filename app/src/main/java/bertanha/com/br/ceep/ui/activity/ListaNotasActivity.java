16a8d83cb78fbd97c6b5e339c124f64daf651ca2
Felipe Bertanha <bertanhahp@hotmail.com>
2018-04-25 23:01:33 -0300

    Aula 01, 02, 03,04

--- app/src/main/java/bertanha/com/br/ceep/ui/activity/ListaNotasActivity.java
+++ app/src/main/java/bertanha/com/br/ceep/ui/activity/ListaNotasActivity.java
@@ -1,16 +1,20 @@
 package bertanha.com.br.ceep.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.List;
 
 import bertanha.com.br.ceep.R;
 import bertanha.com.br.ceep.dao.NotaDAO;
 import bertanha.com.br.ceep.model.Nota;
-import bertanha.com.br.ceep.ui.adapter.ListaNotasAdapter;
+import bertanha.com.br.ceep.ui.recyclerview.adapter.ListaNotasAdapter;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
@@ -18,17 +22,47 @@ public class ListaNotasActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_notas);
+        List<Nota> todasNotas = notasDeExemplo();
+        configuraRecyclerView(todasNotas);
 
-        ListView listaNotas = findViewById(R.id.listView);
+        TextView botaoInsereNota = findViewById(R.id.lista_notas_insere_nota);
+        botaoInsereNota.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ListaNotasActivity.this, FormularioNotaActivity.class));
+            }
+        });
+
+    }
+
+    private List<Nota> notasDeExemplo() {
         NotaDAO dao = new NotaDAO();
+        dao.insere(new Nota("Primeira nota", "Descrição pequena"));
+        dao.insere(new Nota("Segunda nota", "Segunda descrição maior abcdefgh"));
+//        for (int i = 0; i < 10000; i++) {
+//            dao.insere(new Nota("Nota " + i, "Descrição " + i));
+//
+//        }
+        return dao.todos();
+    }
 
-        for (int i = 0; i < 10000; i++) {
-            dao.insere(new Nota("Nota " + i, "Descrição " + i));
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 
-        }
+    private void configuraRecyclerView(List<Nota> todasNotas) {
+        RecyclerView listaNotas = findViewById(R.id.lista_notas_recyclerview);
+        configuraAdapter(todasNotas, listaNotas);
+        //configuraLayoutManager(listaNotas);
+    }
 
-        List<Nota> todasNotas = dao.todos();
+    private void configuraLayoutManager(RecyclerView listaNotas) {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        listaNotas.setLayoutManager(layoutManager);
+    }
 
+    private void configuraAdapter(List<Nota> todasNotas, RecyclerView listaNotas) {
         listaNotas.setAdapter(new ListaNotasAdapter(this, todasNotas));
     }
 }
